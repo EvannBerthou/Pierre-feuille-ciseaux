8cfefc62e5d89548333aa0b7c48772e2a9c566bb
Sutalite <evann.berthou@gmail.com>
2021-08-05 15:22:14 +0200

    Supprission de test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'Premier coup'
-curl -s -X POST localhost:8080/game/1/1/c | jq
-echo -e '\nSecond coup'
-curl -s -X POST localhost:8080/game/1/2/p | jq
-echo -e '\nRésultat'
-curl -s localhost:8080/game/1/ | jq
