d24417e0678deb5ce8e935faaf2c802f154d4d72
Sutalite <evann.berthou@gmail.com>
2021-08-08 15:15:31 +0200

    Back: Ajout de mapping spécifique pour login(POST) et user(GET)

--- Spring/src/main/java/com/example/demo/Controller/UserController.java
+++ Spring/src/main/java/com/example/demo/Controller/UserController.java
@@ -10,9 +10,10 @@ import com.example.demo.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,12 +26,13 @@ public class UserController {
             new BCryptPasswordEncoder().matches(password, details.getPassword());
     }
 
-    @RequestMapping("/login")
+    @PostMapping("/login")
     public boolean login(@RequestBody User user) {
         return isValidLogin(user.getUsername(), user.getPassword());
     }
 
-    @RequestMapping("/user/{username}")
+    //TODO: Vérifier qu'un authToken est présent dans la requête
+    @GetMapping("/user/{username}")
     public User user(HttpServletRequest request, @PathVariable String username) {
 		String authToken = request.getHeader("Authorization").substring("Basic".length()).trim();
 		String[] parts = new String(Base64.getDecoder().decode(authToken)).split(":");
