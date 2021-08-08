15865a87cd51c8df658bce20d01a91cb0bd1b58f
Sutalite <evann.berthou@gmail.com>
2021-08-08 15:13:03 +0200

    Back: Détecter si l'utilisateur regarde son profil ou celui d'un autre

--- Spring/src/main/java/com/example/demo/Controller/UserController.java
+++ Spring/src/main/java/com/example/demo/Controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.demo.Controller;
 
-import java.security.Principal;
 import java.util.Base64;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +10,7 @@ import com.example.demo.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +18,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class UserController {
-    @RequestMapping("/login")
-    public boolean login(@RequestBody User user) {
-        UserDetails details = new MyUserDetailsService().loadUserByUsername(user.getUsername());
-        return details.getUsername().equals(user.getUsername()) && 
-            new BCryptPasswordEncoder().matches(user.getPassword(), details.getPassword());
+
+    private boolean isValidLogin(String username, String password) {
+        UserDetails details = new MyUserDetailsService().loadUserByUsername(username);
+        return details.getUsername().equals(username) && 
+            new BCryptPasswordEncoder().matches(password, details.getPassword());
     }
 
-    @RequestMapping("/user")
-    public Principal user(HttpServletRequest request) {
+    @RequestMapping("/login")
+    public boolean login(@RequestBody User user) {
+        return isValidLogin(user.getUsername(), user.getPassword());
+    }
+
+    @RequestMapping("/user/{username}")
+    public User user(HttpServletRequest request, @PathVariable String username) {
 		String authToken = request.getHeader("Authorization").substring("Basic".length()).trim();
-		return () -> new String(Base64.getDecoder().decode(authToken)).split(":")[0];
+		String[] parts = new String(Base64.getDecoder().decode(authToken)).split(":");
+        String tokenUsername = parts[0];
+        String tokenPassword = parts[1];
+
+        // Si l'utilisateur connecté veut regarder son propre profil
+        if (username.equals(tokenUsername) && isValidLogin(username, tokenPassword)) {
+            return new User("Self", "Profile");
+        } 
+
+        return new User("Other", "Profile");
     }
 }
