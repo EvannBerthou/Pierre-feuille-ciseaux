25f95b85d69b18431044f099f296d0d7d895f08e
Evann Berthou <evann.berthou@gmail.com>
2021-08-23 14:34:36 +0200

    Back: Ajout de la structure pour réaliser les tests

--- Spring/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Spring/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.Controller.GameController;
+import com.example.demo.Controller.UserController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired private GameController gameController;
+    @Autowired private UserController userController;
+
 	@Test
 	void contextLoads() {
+        assertThat(gameController).isNotNull();
+        assertThat(userController).isNotNull();
 	}
 
 }
