6df7e76d079e5824578414201cbd9e8617415ef9
Evann Berthou <evann.berthou@gmail.com>
2021-08-24 16:39:08 +0200

    Front: Ajout de GameProfilesService
    Permet de récupérer le profil de tous les jours d'une partie donnée.
    Cela permet (entre autre) de récupérer l'username des joueurs
    de la partie.

--- Angular/src/app/game-details/game-details.component.ts
+++ Angular/src/app/game-details/game-details.component.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import {Profile} from '../profile';
 import {Game} from '../game';
 import {AuthenticationService} from '../authentication.service';
+import {GameProfilesService} from '../game-profiles.service';
 
 @Component({
   selector: 'app-game-details',
@@ -16,7 +17,7 @@ export class GameDetailsComponent implements OnInit {
     gameData!: Game;
     playerData: Profile[] = [];
 
-    constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private auth: AuthenticationService) { }
+    constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private auth: AuthenticationService, private gameProfiles: GameProfilesService) { }
 
     ngOnInit(): void {
         let paramId = this.activatedRoute.snapshot.params.gameid;
@@ -25,21 +26,11 @@ export class GameDetailsComponent implements OnInit {
         this.gameid = paramId;
 
         const gameUrl = `http://localhost:8080/game/${this.gameid}`;
-        this.http.get<Game>(gameUrl).subscribe((response) => {
+        this.http.get<Game>(gameUrl).subscribe(response => {
             if (response === null) return;
-            console.log(response);
-
             this.gameData = response;
 
-            if (this.gameData.tour1 !== null) {
-                const player1Url = `http://localhost:8080/user/id/${this.gameData.tour1.player}`;
-                    this.http.get<Profile>(player1Url).subscribe(response => this.playerData[0] = response);
-            }
-
-            if (this.gameData.tour2 !== null) {
-                const player2Url = `http://localhost:8080/user/id/${this.gameData.tour2.player}`;
-                this.http.get<Profile>(player2Url).subscribe(response => this.playerData[1] = response);
-            }
+            this.playerData = this.gameProfiles.getGameProfiles(this.gameData);
         });
     }
 
@@ -57,23 +48,25 @@ export class GameDetailsComponent implements OnInit {
     get classUser1() : String { return this.headerClass(this.gameData.tour1?.player); }
     get classUser2() : String { return this.headerClass(this.gameData.tour2?.player); }
     get getResult() { 
-        if (!this.gameData.ended) return "En cours";
-        if (this.gameData.winner === null ) return "Egalité";
+        if (this.gameData.ended === false) return "En cours";
+        if (this.gameData.winner === null) return "Egalité";
 
         const selfUsername = this.auth.username;
-
         if (selfUsername === "") return "Terminée";
 
+        let selfStatus = (player: Number) => {
+            return this.isWinner(player) ? "Gagnant" : "Perdant";
+        }
+
         if (selfUsername == this.username1) {
-            return this.isWinner(this.gameData.tour1.player) ? "Gagnant" : "Perdant";
+            return selfStatus(this.gameData.tour1.player);
         }
 
         if (selfUsername == this.username2) {
-            return this.isWinner(this.gameData.tour2.player) ? "Gagnant" : "Perdant";
+            return selfStatus(this.gameData.tour2.player);
         }
-        return "";
+        return "?";
     }
 
     get getStartDate() { return formatDate(new Date(this.gameData.creationDate), 'yyyy/MM/dd', 'en'); }
-
 }
